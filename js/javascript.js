2b8af799f4236c40a34ac33ffe4f2a887425c074
jennyba-huey <djeneba.ba@epfedu.fr>
2023-12-08 10:18:53 +0100

    Partie projet fait/diapo non fonc fond moche

--- js/javascript.js
+++ js/javascript.js
@@ -5,4 +5,36 @@ menuButton.addEventListener('click', () => {
   menu.classList.toggle('open');
 });
 
+const slide = document.querySelectorAll('.carousel-item');
+let currentIndex = 0;
+
+function showSlide(index) {
+    slide.forEach(item => {
+        item.style.display = 'none';
+    });
+
+    // Faire appraitre et bloquer l'image?
+    slide[index].style.display = 'block';
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slide.length;
+    showSlide(currentIndex);
+}
+
+function previousSlide() {
+    currentIndex = (currentIndex - 1 + slide.length) % slide.length;
+    showSlide(currentIndex);
+}
+
+// Première Image pour l'ouverture du Site
+showSlide(currentIndex);
+document.querySelector('.nextBtn').addEventListener('click', nextSlide);
+document.querySelector('.prevBtn').addEventListener('click', previousSlide);
+// Auto slide change example (uncomment to enable)
+// setInterval(nextSlide, 3000);
+
+
+// Auto slide change example (uncomment to enable)
+// setInterval(nextSlide, 3000);
 
