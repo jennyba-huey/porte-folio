7304f838c0b6418f4d937fa18f4ec9469abd9a93
jennyba-huey <djeneba.ba@epfedu.fr>
2023-12-15 10:37:03 +0100

    supp diapo et new background

--- js/javascript.js
+++ js/javascript.js
@@ -1,40 +1,9 @@
-const menu = document.querySelector('.menu');
-const menuButton = document.getElementById('menu-button'); // Remplacez 'menu-button' par l'ID de votre bouton/menu
-
-menuButton.addEventListener('click', () => {
-  menu.classList.toggle('open');
-});
-
-const slide = document.querySelectorAll('.carousel-item');
-let currentIndex = 0;
-
-function showSlide(index) {
-    slide.forEach(item => {
-        item.style.display = 'none';
-    });
-
-    // Faire appraitre et bloquer l'image?
-    slide[index].style.display = 'block';
+function barremenu() {
+    var x = document.getElementById("idbanniere2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 }
-
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % slide.length;
-    showSlide(currentIndex);
-}
-
-function previousSlide() {
-    currentIndex = (currentIndex - 1 + slide.length) % slide.length;
-    showSlide(currentIndex);
-}
-
-// Première Image pour l'ouverture du Site
-showSlide(currentIndex);
-document.querySelector('.nextBtn').addEventListener('click', nextSlide);
-document.querySelector('.prevBtn').addEventListener('click', previousSlide);
-// Auto slide change example (uncomment to enable)
-// setInterval(nextSlide, 3000);
-
-
-// Auto slide change example (uncomment to enable)
-// setInterval(nextSlide, 3000);
 
